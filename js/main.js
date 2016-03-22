5de7ccb9acd160496230920378609dd917b8b8b2
zlailari <lailari.zeev@gmail.com>
2016-03-22 00:05:15 -0500

    images stick

--- js/main.js
+++ js/main.js
@@ -1,13 +1,28 @@
+var can, ctx;
+
 $(function(){
+    can = document.getElementById('paintCanvas');
+    ctx = can.getContext('2d');
+    can.width = window.innerWidth;
+    can.height = window.innerHeight;
+
+    ctx.fillRect(0, 0, 10000, 10000);
+
     var scroller = $('#scroller div.innerScrollArea');
     var scrollerContent = scroller.children('ul');
     scrollerContent.children().clone().appendTo(scrollerContent);
+
     var curY = 0;
+
     scrollerContent.children().each(function(){
         var $this = $(this);
         $this.css('top', curY);
         curY += $this.outerHeight(true) + 100;
+        this.addEventListener('click', function(e) {
+            stick(this);
+        });
     });
+
     var fullH = curY / 2;
     var viewportH = scroller.height();
 
@@ -34,3 +49,16 @@ $(function(){
         doScroll(e.deltaY);
     });
 });
+
+var stick = function(el) {
+    var dims = el.getBoundingClientRect();
+    var img = $(el).find('img').attr('src');
+
+    // This is extra jank
+    imgload = new Image();
+    imgload.onload = function() {
+        ctx.drawImage(imgload, dims.left, dims.top, dims.width, dims.height);
+    };
+    imgload.src = (img);
+
+}
