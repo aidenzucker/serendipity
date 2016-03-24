abfa14221a83c5826741a33bcc2db9ee55f52ae3
zlailari <lailari.zeev@gmail.com>
2016-03-24 00:54:10 -0500

    Added basic mobile interaction, spaced images nicely, cleaned up code a bit

--- js/main.js
+++ js/main.js
@@ -1,35 +1,65 @@
 var can, ctx;
 
+// Disable pull down to refresh:
+// http://stackoverflow.com/questions/29008194
+var target = window; // this can be any scrollable element
+var last_y = 0;
+target.addEventListener('touchmove', function(e){
+    var scrolly = target.pageYOffset || target.scrollTop || 0;
+    var direction = e.changedTouches[0].pageY > last_y ? 1 : -1;
+    if(direction>0 && scrolly===0){
+        e.preventDefault();
+    }
+    last_y = e.changedTouches[0].pageY;
+});
+
 $(function(){
-    can = document.getElementById('paintCanvas');
-    ctx = can.getContext('2d');
-    can.width = window.innerWidth;
-    can.height = window.innerHeight;
-
-    ctx.fillRect(0, 0, 10000, 10000);
-
-    var scroller = $('#scroller div.innerScrollArea');
-    var scrollerContent = scroller.children('ul');
-    scrollerContent.children().clone().appendTo(scrollerContent);
-
-    var curY = 0;
-
-    scrollerContent.children().each(function(){
-        var $this = $(this);
-        $this.css('top', curY);
-        curY += $this.outerHeight(true) + 100;
-        this.addEventListener('click', function(e) {
-            stick(this);
-        });
-    });
+    var curY, scroller, scrollerContent;
+    init();
 
     var fullH = curY / 2;
     var viewportH = scroller.height();
 
-    // Scrolling speed management
-    var controller = {curSpeed:2, fullSpeed:2};
-    var scrollSpeed = 10;
-    var $controller = $(controller);
+    function init() {
+        can = document.getElementById('paintCanvas');
+        ctx = can.getContext('2d');
+        can.width = window.innerWidth;
+        can.height = window.innerHeight;
+
+        ctx.fillStyle = "#E6E6E6";
+        ctx.fillRect(0, 0, 10000, 10000);
+
+        scroller = $('#scroller div.inner-scroll-area');
+
+        var hammerScroll = new Hammer(scroller[0]);
+        hammerScroll.on('pan', function(e) {
+            doScroll(-e.deltaY / 10);
+        });
+
+        scrollerContent = $('.scroller-content');
+
+        curY = 0;
+        setupImages();
+    }
+
+    function setupImages() {
+        scrollerContent.children().each(function(){
+            var $this = $(this);
+            $this.css('top', curY);
+            curY += $this.outerHeight(true) + 300;
+
+            addImageListeners(this);
+        });
+    }
+
+    function addImageListeners(img) {
+        var hammerImg = new Hammer(img);
+
+        hammerImg.on('tap', function(ev) {
+            stick(img);
+        });
+    }
+
 
     // Scrolling management; start the automatical scrolling
     var doScroll = function(ySpeed)
