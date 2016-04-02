622e7a556ebbf5d8d95689754f0217b3279c911e
Aiden Zucker <aidenzucker@gmail.com>
2016-04-02 16:47:47 -0500

    Good CSS for home and the start of a sticky menu for all pages

--- js/templates.js
+++ js/templates.js
@@ -2,6 +2,6 @@ var headerTemplate=`<header>
     <nav>
       <a href="index.html" class="logo">S</a>
       <a href="learn.html" >Learn</a>
-      <a href="practice.html" >Practice</a>
+      <a href="practice.html" >Juxtagraph</a>
     </nav>
   </header>`;
