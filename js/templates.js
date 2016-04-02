dfe1404be9a4d18d2f79c38d0bebf7e30f376e84
Aiden Zucker <aidenzucker@gmail.com>
2016-04-02 12:24:11 -0500

    tbh idk what I'm adding here

---
+++ js/templates.js
@@ -0,0 +1,7 @@
+var headerTemplate=`<header>
+    <nav>
+      <a href="index.html" class="logo">S</a>
+      <a href="learn.html" >Learn</a>
+      <a href="practice.html" >Practice</a>
+    </nav>
+  </header>`;
