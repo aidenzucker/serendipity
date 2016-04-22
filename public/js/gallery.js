66c8b841eec873991dfa2163f3e5cba6853f98c2
zlailari <lailari.zeev@gmail.com>
2016-04-22 18:08:08 -0500

    Lazy load gallery

---
+++ public/js/gallery.js
@@ -0,0 +1,43 @@
+(function() {
+    var totalCount = 0;
+    var loadAmount = 9;
+    var currentImg = 0;
+
+    $(window).scroll(function(e) {
+        // Lazy Load when reached the bottom
+        if($(window).scrollTop() + $(window).innerHeight() >= $('.gallery')[0].scrollHeight) {
+            loadImageSet();
+        }
+    });
+
+    var temp = "<li><a><img class='gal-img' src=''/></a></li>";
+
+    function loadImageSet() {
+       var c = Math.min(totalCount, currentImg+loadAmount);
+       for (var i = 0; i <= c; i++) {
+           getImage(i);
+       }
+    }
+
+    function getImage(i) {
+        var newImage = $(temp);
+        newImage.find('img').attr("src", '/images/' + i);
+        newImage.find('a').attr("href", '/detail/' + i);
+
+        $('.gallery-list').prepend(newImage);
+    }
+
+    $.ajax({
+        url: '/count',
+        type: 'GET',
+        success: function (data) {
+            totalCount = data.count;
+
+            loadImageSet();
+        },
+         error: function (error) {
+            console.log('fail: ' + data);
+        }
+    });
+
+})()
